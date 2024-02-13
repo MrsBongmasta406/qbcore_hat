4b09d696bada48585c69bbd1ed93f4dcf29216e4
MrsBongmasta406 <111566632+MrsBongmasta406@users.noreply.github.com>
2024-02-13 02:03:13 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
-fx_version ‘cerulean’
-game ‘gta5’
-author ‘Mrs.Bong’
-description ‘QBCore Hat Made By Kakarot’
-version ‘4.2.0’
+fx_version 'cerulean'
+game 'gta5'
+author 'Mrs.Bong'
+description 'QBCore Hat Made By Kakarot'
+version '4.2.0'
 
 files {
     'mp_m_freemode_01_qbcore.meta'
